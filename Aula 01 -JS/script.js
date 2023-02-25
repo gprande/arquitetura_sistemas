382ad1642113fdbd56d3da909c8d759f22d6e327
gprande <bielprande@gmail.com>
2023-02-25 09:41:46 -0300

    Aprendendo JavaScript

---
+++ Aula 01 -JS/script.js
@@ -0,0 +1,57 @@
+//const, var, let
+
+// console.log(true == 1) //true
+// console.log(true =='1') //true
+// console.log(true === "1") //false
+// console.log(true === 1) //false
+
+console.log(null == null) //true
+console.log(null ==undefined) //true
+console.log([] == false) //true
+console.log([] == []) //false
+
+// console.log(1 == 1) //true
+// console.log(1 == '1') //true
+// console.log(1 === "1") //false
+// console.log(1 === 1) //true
+
+// const n1 =2
+// const n2 = '3'
+// //coerção implícita
+// const n3 = n1 + n2
+// //coerção explícita
+// const n4 = n1 + Number(n2)
+
+// var idade = 18
+// console.log('Oi,' + nome)
+// if (idade>=18){
+//     let nome = "João" //içamento hoist
+//     console.log(nome + ", você pode dirigir.")
+// }
+// console.log('Até mais, ' + nome)
+
+
+// var linguagem = "JavaScript"
+// console.log("Aprendendo " + linguagem)
+// var linguagem = "Java"
+// console.log("Aprendendo " + linguagem)
+
+//@Deprecated;
+
+//Thread.stop()
+
+//let a = 2
+//let b = "abc"
+//console.log(typeof(b))
+//b = 12
+//console.log(typeof(b))
+//b = true
+//console.log(typeof(b))
+
+//const nome1 = "Ana"
+//const nome2 ='Pedro'
+//const nome3 = `Gustavo`
+//console.log(typeof(nome1))
+
+//REPL: Read, Evaluate, Print, Loop
+
