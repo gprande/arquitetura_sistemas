c04c1b23578fedac834b4a643712c54693b5483f
gprande <bielprande@gmail.com>
2023-03-04 09:10:49 -0300

    Funções, Arrays e Arrow Functions

---
+++ Aula 02 -JS/script.js
@@ -0,0 +1,79 @@
+// vetores
+
+// v1=[]
+// v1[0]=3.4
+// v1[10]=2
+// console.log(v1)
+
+// for (let i=0; i<v1.length;i++)
+//     console.log(v1[i])
+
+// arrow function
+// const teste = () => {console.log("My arrow function")}
+// teste()
+
+// // filter
+// const nomes = ['Ana Maria', 'Antonio','Rodrigo','Claudio']
+// const resultados = nomes.filter((nome)=>{return nome.startsWith('A')})
+// console.log(resultados)
+
+// // mapeamento
+// const champs = ['Aatrox', 'Nunu','Assol','Lissandra']
+// const initialLetters = champs.map((nome)=>{return nome.charAt(0)})
+// console.log(initialLetters)
+
+// const numeros = [1,2,3,4,5]
+// const potencia = numeros.map((number)=>{return number**2})
+// console.log(potencia)
+
+// const values = [1,2,3,4]
+// const soma = values.reduce((ac,v)=>{return ac + v})
+// console.log(soma)
+
+// odd number
+// const values = [1,2,3,4]
+// const odd = values.filter((number)=>{return number % 2 !== 0})
+// console.log(odd)
+
+// even number
+// const values = [1,2,3,4]
+// const even = values.filter((number)=>{return number % 2 === 0})
+// console.log(even)
+
+// function soma(a,b){
+//     return a+b;
+// }
+// console.log(soma(2,3))
+
+// v=[2,'abc',true]
+
+// //functions
+// function hello(){
+//     console.log('Hello')
+// }
+// hello()
+// //  içamento ~ var
+// function hello(nome){
+//     console.log('Hello ' + nome)
+// }
+// hello('Flávia')
+
+// const dobro = function (n){
+//     return 2 * n
+// }
+// const res = dobro(5)
+// console.log(res)
+
+// valor padrao para um parametro
+// const triple = function (n=5){
+//     return n*3
+// }
+// console.log(triple())
+// console.log(triple(214))
+
+// (a,b) => console.log(a+b) //syntax sugar
+// const test = (a,b,c) => {return a * b * c}
+// const tests = (a,b,c) =>  a * b * c //use return if u use {}
+
+// const dobro = valor => valor * 2
+
