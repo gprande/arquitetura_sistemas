c5703eed235c08e6a0fe78c95405c4c4b9328729
Prandinho <bielprande@gmail.com>
2023-03-25 09:18:00 -0300

    APPID Security

--- Aula 05 -JS/forecast.js
+++ Aula 05 -JS/forecast.js
@@ -1,17 +1,15 @@
-// 21f032ecda9f5327a1a064da77f034c9
-
-const appid = '21f032ecda9f5327a1a064da77f034c9'
-
+require('dotenv').config()
+const axios = require('axios')
+const appid = process.env.APPID
 const q = 'Sorocaba'
-
 const cnt = 5
-
 const units = 'metric'
-
 const lang = 'pt_br'
-
-const urlBase ='api.openweathermap.org/data/2.5/forecast'
-
-const url = `${urlBase}?q=${q}&appid=${appid}&lang=${lang}`
+const urlBase = 'api.openweathermap.org/data/2.5/forecast'
+const url = `${urlBase}?q=${q}&appid=${appid}&lang=${lang}&cnt=${cnt}&units=${units}`
 
 console.log(url)
+
+axios.get(url).then(response => {
+    console.log(response);   
+})
