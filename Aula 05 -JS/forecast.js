78a9542102e4198784d26745f2082449f75bf152
Prandinho <bielprande@gmail.com>
2023-03-25 08:51:52 -0300

    Weather forecast

---
+++ Aula 05 -JS/forecast.js
@@ -0,0 +1,17 @@
+// 21f032ecda9f5327a1a064da77f034c9
+
+const appid = '21f032ecda9f5327a1a064da77f034c9'
+
+const q = 'Sorocaba'
+
+const cnt = 5
+
+const units = 'metric'
+
+const lang = 'pt_br'
+
+const urlBase ='api.openweathermap.org/data/2.5/forecast'
+
+const url = `${urlBase}?q=${q}&appid=${appid}&lang=${lang}`
+
+console.log(url)
